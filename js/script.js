708c80189fcf8ad02fb17b2179ba000ac965c479
roziier <rikirosa9@gmail.com>
2020-12-10 12:04:32 +0100

    start footer

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var app = new Vue ({
+  el: '#app',
+  data: {
+    logoImg: 'logo.png',
+    arrayLinkFoot: ['@ COPYRIGHT 2012-2020', 'AVADA THEME BY THEMEFUSION', 'ALL RIGHT RESERVED', 'POWERED BY WORDPRESS']
+  }
+})
