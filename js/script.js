3167ab175085b39dc97cf34fdf7bfa5473dfc3d4
roziier <rikirosa9@gmail.com>
2020-12-10 13:33:35 +0100

    footer finished

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ var app = new Vue ({
   el: '#app',
   data: {
     logoImg: 'logo.png',
-    arrayLinkFoot: ['@ COPYRIGHT 2012-2020', 'AVADA THEME BY THEMEFUSION', 'ALL RIGHT RESERVED', 'POWERED BY WORDPRESS']
+    arrayHref: ['https://www.google.it/'],
+    arrayLinkFoot: ['@ COPYRIGHT 2012-2020', 'AVADA THEME BY THEMEFUSION', 'ALL RIGHT RESERVED', 'POWERED BY WORDPRESS'],
+    arrayIconFooter: ['fa-facebook-f', 'fa-twitter', 'fa-instagram', 'fa-youtube']
   }
 })
